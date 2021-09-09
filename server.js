3217ba6a7c7586f6cbd558253f8bf9dc4872c60e
Samantha Marah <samanthamarah140@gmail.com>
2021-09-09 16:25:09 +0100

    commit

--- server.js
+++ server.js
@@ -21,14 +21,14 @@ const forceSSL = function() {
 app.use(forceSSL());
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist/ibatmovie'));
+app.use(express.static(__dirname + '/dist/acmeapp'));
 
 
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/ibatmovie/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/acmeapp/index.html'));
 });
 // Start the app by listening on the default
 // Heroku port
